4a541a6727d7b74b20aeeab244094a9152bee0f9
Zachary Ackerman <zackerman24@gmail.com>
2019-09-04 20:50:03 -0400

    Notes to self

--- tbd.py
+++ tbd.py
@@ -9,3 +9,12 @@ Created on Wed Sep  4 20:12:40 2019
 print("\nHello")
 print("Why so many lines?")
 input("Give me a number")
+
+"""
+Every type of ship might require a class?
+But each ship probably needs a dictionary to track the places that they
+currently occupy
+Then run through all dictionaries to see if you struck any
+If so, remove that spot from the dictionary - or create new dictionary with
+spots that have been hit so you can respond differently
+"""
